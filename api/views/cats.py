9cf9a88ea4bae682d3146878b9b7c9e2546f853b
provideforme <evan.a.bernard@gmail.com>
2023-02-14 16:50:50 -0500

    create cats view

--- api/views/cats.py
+++ api/views/cats.py
@@ -5,3 +5,14 @@ from api.models.db import db
 from api.models.cat import Cat
 
 cats = Blueprint('cats', 'cats')
+
+@cats.route('/', methods=["POST"])
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data["profile_id"] = profile["id"]
+  cat = Cat(**data)
+  db.session.add(cat)
+  db.session.commit()
+  return jsonify(cat.serialize()), 201

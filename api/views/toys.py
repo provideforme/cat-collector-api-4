2aee70256dc759f2d783f1d4f25afc4e3d9fbf38
provideforme <evan.a.bernard@gmail.com>
2023-02-15 12:20:41 -0500

    create toy

--- api/views/toys.py
+++ api/views/toys.py
@@ -5,3 +5,15 @@ from api.models.db import db
 from api.models.toy import Toy
 
 toys = Blueprint('toys', 'toys')
+
+@toys.route('/', methods=["POST"])
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data["profile_id"] = profile["id"]
+
+  toy = Toy(**data)
+  db.session.add(toy)
+  db.session.commit()
+  return jsonify(toy.serialize()), 201
